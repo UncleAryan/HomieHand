94e087ec7833a74daad8ac59232e4c0f13ca428b
UncleAryan <aryanpanchal1702@gmail.com>
2023-12-23 21:42:17 -0500

    Implemented Optimized Animations

--- Homie Hand/src/assets/BigPlayer.java
+++ Homie Hand/src/assets/BigPlayer.java
@@ -3,7 +3,6 @@ package assets;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
-import java.awt.image.BufferedImage;
 import java.util.LinkedList;
 
 import framework.AnimationLoader;
