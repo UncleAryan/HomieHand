3c9705c71773c3e7dc625cb59ec0d4963c02fcd1
System Name <aryanpanchal1702@gmail.com>
2026-08-16 21:27:29 -0400

    Separate runnable for level editor

--- src/core/LevelEditor.java
+++ src/core/LevelEditor.java
@@ -7,14 +7,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class LevelEditor {
+public class LevelEditor extends JPanel implements Runnable {
     private JFrame frame;
     private JPanel panel;
     private BufferedImage level;
+    private Thread thread;
 
     public LevelEditor() {
         loadWindow();
+        startThread();
         loadLevel();
+    }
+
+    private void startThread() {
+        thread = new Thread(this);
+        thread.start();
     }
 
     private void loadLevel() {
@@ -22,7 +29,9 @@ public class LevelEditor {
 
     }
 
-    public void render(Graphics g) {
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
         g.drawImage(level, 0, 0, null);
     }
 
@@ -32,12 +41,24 @@ public class LevelEditor {
 
     private void loadWindow() {
         frame = new JFrame("Level Editor");
-        panel = new JPanel();
-        panel.setPreferredSize(new Dimension((int) Constants.WIDTH , (int)Constants.HEIGHT));
-        frame.add(panel);
+        this.setPreferredSize(new Dimension((int) Constants.WIDTH , (int)Constants.HEIGHT));
+        frame.add(this);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+
+    public void run() {
+        while(true) {
+            try {
+                repaint();
+                tick();
+
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
